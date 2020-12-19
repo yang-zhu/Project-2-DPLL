c05cd14b75bee408fdf6aa0878aa0314b13cf0a6
Zhu Yang <zhuyang329@gmail.com>
2020-12-19 22:09:29 +0100

    did minor changes, added some comments

--- dpll_solver.cpp
+++ dpll_solver.cpp
@@ -22,24 +22,30 @@ enum class Value {
     unset, f, t
 };
 
+// Distinguish between forced and branching literals.
+enum class Mark {
+    forced, branching
+};
+
 struct Variable {
     Value value = Value::unset;
     vector<Clause*> pos_occ;
     vector<Clause*> neg_occ;
-    void set(Value);
+    void set(Value, Mark);
     void unset();
 };
 
 vector<Variable> variables;
-deque<Clause> clauses;  // Use deque instead of vector to avoid dangling pointers
-vector<pair<Variable*, char>> assignments;
-vector<Clause*> unit_queue;
+deque<Clause> clauses;  // uses deque instead of vector to avoid dangling pointers
+vector<pair<Variable*, Mark>> assignments;
+vector<Clause*> unit_clauses;
 
 void backtrack();
 
 // Assign truth value to a variable.
-void Variable::set(Value value) {
-    this->value = value;
+void Variable::set(Value new_value, Mark mark) {
+    assignments.push_back(make_pair(this, mark));
+    value = new_value;
     bool found_conflict = false;
     for (Clause* cl: (value == Value::t) ? pos_occ : neg_occ) {
         if (cl->sat_var == nullptr) {
@@ -50,7 +56,7 @@ void Variable::set(Value value) {
         if (cl->sat_var == nullptr) {
             cl->active -= 1;
             if (cl->active == 1) {
-                unit_queue.push_back(cl);
+                unit_clauses.push_back(cl);
             } else if (cl->active == 0) {
                 found_conflict = true;
             }
@@ -71,7 +77,7 @@ void Variable::unset() {
             cl->active += 1;
         }
     }
-    this->value = Value::unset;
+    value = Value::unset;
 };
 
 void fromFile(string path) {
@@ -94,7 +100,7 @@ void fromFile(string path) {
         file >> cnf >> num_vars >> num_clauses;
         
         // Fill the vector of variables.
-        variables.push_back(Variable());
+        variables.push_back(Variable());  // to allow indexing of variables to start from 1
         for (int i = 1; i < num_vars+1; ++i) {
             variables.push_back(Variable());
         }
@@ -102,16 +108,16 @@ void fromFile(string path) {
         // Fill the deque of clauses.
         for (int i = 0; i < num_clauses; ++i) {
             vector<int> lits;
-            int var;
-            file >> var;
-            while (var != 0) {
-                lits.push_back(var);
-                file >> var;
+            int lit;
+            file >> lit;
+            while (lit != 0) {
+                lits.push_back(lit);
+                file >> lit;
             }
             clauses.push_back(Clause(lits, lits.size()));
+
             Clause* cl = &clauses[i];
-            
-            if (cl->active == 1) {unit_queue.push_back(cl);}
+            if (cl->active == 1) {unit_clauses.push_back(cl);}
 
             for (int lit: lits) {
                 if (lit > 0) { 
@@ -128,18 +134,16 @@ void fromFile(string path) {
 
 // Unit propagation
 void unit_prop() {
-    while (!unit_queue.empty()) {
-        Clause* cl = unit_queue.back();
-        unit_queue.pop_back();
+    while (!unit_clauses.empty()) {
+        Clause* cl = unit_clauses.back();
+        unit_clauses.pop_back();
         for (int lit: cl->lits) {
             Variable* var = &variables[abs(lit)];
-            if (var->value == Value::unset) {
+            if (var->value == Value::unset) {  // a clause does not keep track of which literals are unassigned
                 if (lit > 0) {
-                    assignments.push_back(make_pair(var, 'f'));
-                    var->set(Value::t);
+                    var->set(Value::t, Mark::forced);
                 } else {
-                    assignments.push_back(make_pair(var, 'f'));
-                    var->set(Value::f);
+                    var->set(Value::f, Mark::forced);
                 }
                 break;
             }
@@ -149,17 +153,16 @@ void unit_prop() {
 
 // Backtracking
 void backtrack() {
-    unit_queue.clear();
+    unit_clauses.clear();
     while (!assignments.empty()) {
         Variable* var = assignments.back().first;
-        char mark = assignments.back().second;
+        Mark mark = assignments.back().second;
         assignments.pop_back();
-        if (mark == 'f') {var->unset();}
+        if (mark == Mark::forced) {var->unset();}
         else {
             Value old_value = var->value;
             var->unset();
-            assignments.push_back(make_pair(var, 'f'));
-            var->set((old_value == Value::t) ? Value::f : Value::t);
+            var->set((old_value == Value::t) ? Value::f : Value::t, Mark::forced);
             return;
         }
     }
@@ -171,11 +174,11 @@ int main(int argc, const char *argv[]) {
     fromFile(argv[1]);
     unit_prop();
     while (true) {
+        // We have to go through the variables many times, because set variables could be unset again by backtracking.
         for (int i = 1; i < variables.size(); ++i) {
             Variable* var = &variables[i];
             if (var->value == Value::unset) {
-                assignments.push_back(make_pair(var, 'b'));
-                var->set(Value::t);
+                var->set(Value::t, Mark::branching);
                 unit_prop();
             }
         }
